579cc3843e7b82d2331c1bcc239c2bd26a36b52f
tuleshprominentpixel <tulesh.g@prominentpixel.com>
2021-10-28 18:50:25 +0530

    ch2ex3

---
+++ 2/ch2ex3/src/app/example-angular/example-angular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'example-angular',
+  templateUrl: './example-angular.component.html',
+  styleUrls: ['./example-angular.component.css']
+})
+export class ExampleAngularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
